a53fcb9be90c3387d7602e985eac1021b73ff0fd
macourage18 <macourage17@gmail.com>
2023-12-08 19:19:11 +0100

    first commit

---
+++ src/pages/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import "./nav.css"
+import { Link } from "react"
+
+export const Navbar = ()=>{
+  return(
+    <div>
+    
+      <nav className="navbar">
+       <div className="logo">
+        <img src={require()} alt="" />
+       </div>
+       <div className="mid">
+
+          <Link to={"/"}>Home</Link>
+          <Link to={"/Service"}>Service</Link>
+
+          <Link to={"/About"}>About</Link>
+        </div>
+        <div>
+          <Link to={"/Contact"}>
+            <button className="btn1">
+              contact
+            </button>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  )
+}
---
+++ src/pages/Service/Service.js
@@ -0,0 +1,7 @@
+export const Service = ()=>{
+  return(
+    <div>
+      
+    </div>
+  )
+}
