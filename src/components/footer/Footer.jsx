e50234925178195e71037cc091c4f807cf73408a
macourage18 <macourage17@gmail.com>
2023-12-20 16:25:43 +0100

    latset

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import BMP from "../media/bmp innovationsz 1.png"
 import "./footer.css"
 import { Link } from "react-router-dom"
-import { Whatsapp, Facebook,Linkedin,Twitter, Apple, GooglePlay } from "react-bootstrap-icons"
+import { Whatsapp, Facebook,Linkedin,Twitter} from "react-bootstrap-icons"
 export const Footer =() =>{
   const path = ()=>{
     window.location.pathname("/")
@@ -34,45 +34,21 @@ export const Footer =() =>{
           </div>
           </div>
         <div className={"bottom3"}>
-          <ul>
-            <h3>BMPInovations</h3>
-            <p>Graphic  Design</p>
-            <p>Logo Design</p>
-            <p>Advert Designs</p>
-            <p>Stationery Design</p>
-            <p>Video & Animation</p>
-            <p>Advertisement</p>
-            <p>Courses</p>
-            <p>Web Development</p>
-            <p>App Development</p>
-          </ul>
+          
           <ul>
           <h3>Quick Links</h3>
             <Link to={"/Service"} onClick={path2}><p>Our Services</p></Link>
             <Link to={"/Contact"} onClick={path4}><p>Contact</p></Link>
             <Link to={"/About"}onClick={path3}><p>About</p></Link>
-            <Link to={"/Pricing"}> <p>Pricing Plans</p></Link>
+    
           </ul>
-          <div className={"contact2"}>Service
+          <div className={"contact2"}>
             <div className={"customer"}>
               <h5>Customer Care</h5>
              <div>
              <Link to={""}><Whatsapp className={"icon4"} /></Link>
               <Link to={""}>bmpinovations@gmail.com</Link>
              </div>
-            </div>
-            <div className={"buttns"}>
-            <button className={"btn5"} >
-              <Apple /> downoad app
-            </button>
-            <button className={"btn5"} >
-              <GooglePlay /> downoad app
-            </button>
-            </div>
-            <div className={"buttns2"}>
-              <button className={"btn6"}>
-                coming soon
-              </button>
             </div>
           </div>
         </div>
